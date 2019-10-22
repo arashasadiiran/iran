9c6b027612ab483996cc896854175ce84cb5a5aa
arash asadi <arashasadi.313.iran@gmail.com>
2019-10-22 06:31:15 +0330

    v,sfjkjs;d

---
+++ arash.py
@@ -0,0 +1,20 @@
+,fgksjgs
+g,sjgsmrntjbtrjefdlb
+rtbnkjrtjrtb
+bnrtjbkjndjbnreb
+d,sfbnmdsfbksfb
+bmdsfnbkdfsbsjr bd
+bdfmbkfdsbkjsfbrb
+sdbjksfdbnjtkrb kfdb
+fdbnjkfdnbjmbtrjknbtrlbjnbmdfsb
+sfdmbjktrbm nfdkbb 
+,skjgs
+dsgkdsjgksiojkmreioesdkjkgds
+:qwi
+
+
+:
+
+
+
+
